e0b2ff92479d9146349d1444017659d2c58a77c7
qwqtw <qiutianw@gmail.com>
2024-09-18 22:46:18 -0400

    Update SecurityConfig.java

--- src/main/java/com/example/linkedinclone/securingweb/SecurityConfig.java
+++ src/main/java/com/example/linkedinclone/securingweb/SecurityConfig.java
@@ -2,10 +2,13 @@ package com.example.linkedinclone.securingweb;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -26,6 +29,12 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 
     @Bean
     public UserDetailsService userDetailsService() {
