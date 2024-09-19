76b14912befc6540c09beb4f6c5d00efe9006831
lkjca <161098836+lkjca@users.noreply.github.com>
2024-09-18 22:05:02 -0400

    Add User.java

---
+++ src/main/java/com/example/linkedinclone/entity/User.java
@@ -0,0 +1,43 @@
+package Entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @NotBlank(message = "Email is required")
+    @Column(unique = true, length=320)
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character")
+    private String password;
+
+    @Transient
+    @NotBlank(message = "Please confirm your password")
+    private String password2;
+
+    @Pattern(regexp = "user|recruiter", message = "Role must be either 'user' or 'recruiter'")
+    private String role;
+
+}
+
