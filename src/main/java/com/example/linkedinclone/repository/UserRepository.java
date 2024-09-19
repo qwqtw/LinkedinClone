15129726f2a8545a3eae7aff589207e1ee38e91c
Carlobunn-123 <151385151+Carlobunn-123@users.noreply.github.com>
2024-09-19 14:56:34 -0400

    Update UserRepository.java

--- src/main/java/com/example/linkedinclone/repository/UserRepository.java
+++ src/main/java/com/example/linkedinclone/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.example.linkedinclone.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@SpringBootApplication(scanBasePackages = {"com.example.linkedinclone"})
 @Repository
 public interface UserRepository  extends JpaRepository<User, Long> {
     User findByUsername(String username);
