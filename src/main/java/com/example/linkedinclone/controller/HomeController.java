04d7a02ef459babc4603e2e551dd4a67c0b6fa04
qwqtw <qiutianw@gmail.com>
2024-09-19 23:52:36 -0400

    post

--- src/main/java/com/example/linkedinclone/controller/HomeController.java
+++ src/main/java/com/example/linkedinclone/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.linkedinclone.controller;
 
+import com.example.linkedinclone.entity.Post;
 import com.example.linkedinclone.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,19 +8,27 @@ import org.springframework.ui.Model;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.example.linkedinclone.service.UserService;
+import com.example.linkedinclone.repository.PostRepository;
 
 import java.security.Principal;
+import java.util.List;
 
 @Controller
 public class HomeController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @GetMapping("/home")
     public String home(HttpServletRequest request, Model model, Principal principal) {
         User user = userService.findByUsername(principal.getName());
+        List<Post> posts= postRepository.findAll();
+
         model.addAttribute("user", user);
         model.addAttribute("currentURI", request.getRequestURI());
+        model.addAttribute("posts", posts);
 //        model.addAttribute("connections", connectionService.findConnectionsByUser(user));
 //        model.addAttribute("suggestedJobs", jobService.findSuggestedJobsForUser(user));
         return "index";
