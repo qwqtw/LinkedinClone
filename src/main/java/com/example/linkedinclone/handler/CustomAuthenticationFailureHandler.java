024ead9bf5204bfd75e0435326dceafff3def51a
lkjca <161098836+lkjca@users.noreply.github.com>
2024-09-22 15:35:26 -0400

    Create CustomAuthenticationFailureHandler.java

---
+++ src/main/java/com/example/linkedinclone/handler/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,19 @@
+package com.example.linkedinclone.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+
+@Slf4j
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        log.info("Login Failed username:" + request.getParameter("username") + " exception: " + exception.getMessage());
+        response.sendRedirect("/login?error=true");
+    }
+}
